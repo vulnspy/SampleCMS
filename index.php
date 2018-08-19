ae4390795aa1b81c79df16b4818edde7c7bc3b8c
Ambulong <zeng.ambulong@gmail.com>
2018-08-19 21:28:08 +0800

    u

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<a href="phpinfo.php">phpinfo</a>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN" lang="zh-CN">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="author" content="Ambulong" />
+  <title>VULNSPY SAMPLE CMS</title>
+  <style>
+    body{
+       font-family: "Menlo", "Meslo LG", monospace;
+       max-width: 50em;
+       color: #444;
+       width: 100%;
+       margin: 0 auto;
+       font-size: 1.2em;
+       line-height: 1.5em;
+    }
+    h1 {
+    	text-align: center;
+    	font-size: 8em;
+    	padding: 0;
+    	margin: 0.2em 0;
+    }
+    .B1 {
+    	text-align: center;
+    	padding: 0 1em;
+    }
+  </style>
+</head>
+<body>
+  <h1><img style="width:2em;" src="/logo.png" alt="jiuwu logo"/></h1>
+  <div class="B1">
+    <p><b>VULNSPY SAMPLE CMS | <a href="http://www.vulnspy.com">WWW.VULNSPY.COM</a></b></p>
+    <p>PHPINFO：<a href="phpinfo.php">/phpinfo.php</a></p>
+    <p>Reddit/讨论版: <a href="https://reddit.com/r/jiuwu/">https://reddit.com/r/jiuwu/</a></p>
+    <p>Tech Blog/技术博客: <a href="https://blog.vulnspy.com">https://blog.vulnspy.com</a></p>
+  </div>
+</html>
+
